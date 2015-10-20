3e3d92c87edc6c307a5490ad5d48f32f345b1883
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-20 17:21:18 -0500

    #2, documents mapped from databases, using them as beans

---
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/domain/Pollution.groovy
@@ -0,0 +1,14 @@
+package mx.ipn.ambienta2mx.hardAnt.domain
+
+/**
+ * Created by alberto on 16/10/15.
+ */
+@groovy.transform.Canonical
+class Pollution{
+    Double airQuality
+    Double ozone
+    Double sulphurDiode
+    Double nitrogenDioxide
+    Double carbonMonoxide
+    Double uv
+}
