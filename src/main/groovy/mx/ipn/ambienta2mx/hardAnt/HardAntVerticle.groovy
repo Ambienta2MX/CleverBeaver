fc5aa16acda4edd855d743ebabddeb5715a8cdcf
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-20 17:22:08 -0500

    #2, Generating pollution and weather services, for querying and parsing information from the ddatabase

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/HardAntVerticle.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/HardAntVerticle.groovy
@@ -1,7 +1,7 @@
 package mx.ipn.ambienta2mx.hardAnt
 
 import mx.ipn.ambienta2mx.hardAnt.verticles.PollutionVerticle
-import mx.ipn.ambienta2mx.hardAnt.verticles.weather.WeatherVerticle
+import mx.ipn.ambienta2mx.hardAnt.verticles.WeatherVerticle
 import org.vertx.groovy.platform.Verticle
 
 class HardAntVerticle extends Verticle {
@@ -11,18 +11,22 @@ class HardAntVerticle extends Verticle {
         definedConfiguration = container.getConfig();
         container.logger.info("Hard Ant verticle started");
         // MX 1 databas
-        container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo1)
+        //container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo1)
         // MX 2 databas
-        container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo2)
+        //container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo2)
         // MX 3 databas
-        container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo3)
+        //container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo3)
         // MX 4 databas
-        container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo4)
+        //container.deployModule('io.vertx~mod-mongo-persistor~2.1.0', definedConfiguration.mongo4)
         // Pollution verticle service
-        container.deployVerticle("groovy:" + PollutionVerticle.class.getCanonicalName(), definedConfiguration)
+        //container.deployVerticle("groovy:" + PollutionVerticle.class.getCanonicalName(), definedConfiguration)
         // Google Maps Solver verticle
-        container.deployVerticle("groovy:" + WeatherVerticle.class.getCanonicalName(), definedConfiguration)
-    }
+        //container.deployVerticle("groovy:" + WeatherVerticle.class.getCanonicalName(), definedConfiguration)
+        /*
+        * Testing
+        * */
+        vertx.eventBus.send("Ambienta2MX.FastEagle.EventBus.Location")
+     }
 
     def stop() {
         container.logger.info("Hard Ant main verticle has stopped!");
---
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/WeatherService.groovy
@@ -0,0 +1,7 @@
+package mx.ipn.ambienta2mx.hardAnt.services
+
+/**
+ * Created by alberto on 16/10/15.
+ */
+class WeatherService {
+}
