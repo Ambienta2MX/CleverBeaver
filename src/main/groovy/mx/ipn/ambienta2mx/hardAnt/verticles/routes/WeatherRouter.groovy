1d4142630365c860a92e137bce657f297e0d47f9
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-20 23:51:06 -0500

    #2, Updating route response, now it works with FasteEagle integration

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/routes/WeatherRouter.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/routes/WeatherRouter.groovy
@@ -23,10 +23,10 @@ class WeatherRouter {
             def place = new JsonSlurper().parse(urlObject)
             if (place) {
                 def mongoOperation = [action: 'save', collection: 'Weather']
+                weatherMap.location = place[0].location;
                 mongoOperation.document = weatherMap;
                 def database = definedConfiguration.states[place[0].state];
                 eventBus.send("${definedConfiguration.databasesAddress}.${database}", mongoOperation) { result ->
-                    println "Se guardó! o hizo algo en la base XP ";
                     if (result) {
                         request.response.end("${JsonOutput.toJson(weatherMap)}")
                     } else {
@@ -40,6 +40,30 @@ class WeatherRouter {
     } as groovy.lang.Closure
 
     def findWeatherByLatLon = { request ->
-        request.response.end("${JsonOutput.toJson(request.query)}");
+        request.bodyHandler { body ->
+            def weatherMap = new JsonSlurper().parseText("$body")
+            def fastEagleService = definedConfiguration.fastEagleService
+            def url = fastEagleService.host + ":" + fastEagleService.port + fastEagleService.url
+            url = url.replace(":latitude", "$weatherMap.latitude")
+            url = url.replace(":longitude", "$weatherMap.longitude")
+            url = url.replace(":maxDistance", "${request.params.maxDistance ?: 100}")
+            def urlObject = new URL(url)
+            def place = new JsonSlurper().parse(urlObject)
+            if (place) {
+                def mongoOperation = [action: 'save', collection: 'Weather']
+                weatherMap.location = place[0].location;
+                mongoOperation.document = weatherMap;
+                def database = definedConfiguration.states[place[0].state];
+                eventBus.send("${definedConfiguration.databasesAddress}.${database}", mongoOperation) { result ->
+                    if (result) {
+                        request.response.end("${JsonOutput.toJson(weatherMap)}")
+                    } else {
+                        request.response.code = 404;
+                        request.response.end("{'save':false}")
+                    }
+
+                }
+            }
+        }
     } as groovy.lang.Closure
 }
