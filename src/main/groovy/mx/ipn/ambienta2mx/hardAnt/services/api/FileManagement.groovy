7f81ad956cbb26e187bfc2feee94b4a35646103f
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-11-26 23:02:14 -0600

    #6, Updating FileManagement interface

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/api/FileManagement.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/api/FileManagement.groovy
@@ -4,6 +4,6 @@ package mx.ipn.ambienta2mx.hardAnt.services.api
  * Created by alberto on 19/11/15.
  */
 interface FileManagement {
-    def generateJsonFile();
-    def generateCSVFile();
+    def generateJsonFile(ArrayList array);
+    def generateCSVFile(ArrayList array);
 }
