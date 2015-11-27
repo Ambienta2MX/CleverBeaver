d9a57190044f13d58d9f0d4153fd145becb3bedd
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-11-26 23:02:39 -0600

    #6, Generating Pollution and WeatherService via Verticles

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/PollutionService.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/PollutionService.groovy
@@ -1,39 +1,39 @@
 package mx.ipn.ambienta2mx.hardAnt.services
 
-<<<<<<< Updated upstream
+
 import org.vertx.groovy.platform.Verticle
-=======
 import mx.ipn.ambienta2mx.hardAnt.services.api.FileManagement
->>>>>>> Stashed changes
 
 /**
  * Created by alberto on 16/10/15.
  */
-<<<<<<< Updated upstream
-class PollutionService extends Verticle {
+
+class PollutionService extends Verticle implements FileManagement {
+
     Map definedConfiguration
     def eventBus
 
+    @Override
     def start() {
         definedConfiguration = container.getConfig()
         eventBus = vertx.eventBus
-        eventBus.registerHandler("$definedConfiguration.WeatherFinder.address") { message ->
+        eventBus.registerHandler("$definedConfiguration.PollutionFinder.address") { message ->
             println "Pollution Verticle working";
             eventBus.send("${definedConfiguration.databasesAddress}.MX1", message.body) { mx1 ->
                 eventBus.send("${definedConfiguration.databasesAddress}.MX2", message.body) { mx2 ->
                     eventBus.send("${definedConfiguration.databasesAddress}.MX3", message.body) { mx3 ->
                         eventBus.send("${definedConfiguration.databasesAddress}.MX4", message.body) { mx4 ->
                             List allPlaces = []
-                            for(element in mx1.body.results) {
+                            for (element in mx1.body.results) {
                                 allPlaces.add(element)
                             }
-                            for(element in mx2.body.results) {
+                            for (element in mx2.body.results) {
                                 allPlaces.add(element)
                             }
-                            for(element in mx3.body.results) {
+                            for (element in mx3.body.results) {
                                 allPlaces.add(element)
                             }
-                            for(element in mx4.body.results) {
+                            for (element in mx4.body.results) {
                                 allPlaces.add(element)
                             }
                             message.reply([results: allPlaces]);
@@ -45,21 +45,18 @@ class PollutionService extends Verticle {
         };
     }
 
+    @Override
     def stop() {
         container.logger.error(this.class.name + "has been stopped");
     }
 
-=======
-class PollutionService implements FileManagement{
-
     @Override
-    def generateJsonFile() {
+    def generateJsonFile(ArrayList array) {
         return null
     }
 
     @Override
-    def generateCSVFile() {
+    def generateCSVFile(ArrayList array) {
         return null
     }
->>>>>>> Stashed changes
 }
--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/WeatherService.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/WeatherService.groovy
@@ -2,18 +2,77 @@ package mx.ipn.ambienta2mx.hardAnt.services
 
 import mx.ipn.ambienta2mx.hardAnt.services.api.FileManagement
 import mx.ipn.ambienta2mx.hardAnt.verticles.routes.WeatherRouter
+import org.vertx.groovy.platform.Verticle
 
 /**
  * Created by alberto on 16/10/15.
  */
-class WeatherService implements FileManagement{
+class WeatherService extends Verticle implements FileManagement {
+
+    Map definedConfiguration
+    def eventBus
+
     @Override
-    def generateJsonFile() {
+    def start() {
+        definedConfiguration = container.getConfig()
+        eventBus = vertx.eventBus
+        eventBus.registerHandler("$definedConfiguration.WeatherFinder.address") { message ->
+            println "Pollution Verticle working";
+            eventBus.send("${definedConfiguration.databasesAddress}.MX1", message.body) { mx1 ->
+                eventBus.send("${definedConfiguration.databasesAddress}.MX2", message.body) { mx2 ->
+                    eventBus.send("${definedConfiguration.databasesAddress}.MX3", message.body) { mx3 ->
+                        eventBus.send("${definedConfiguration.databasesAddress}.MX4", message.body) { mx4 ->
+                            List allPlaces = []
+                            for (element in mx1.body.results) {
+                                allPlaces.add(element)
+                            }
+                            for (element in mx2.body.results) {
+                                allPlaces.add(element)
+                            }
+                            for (element in mx3.body.results) {
+                                allPlaces.add(element)
+                            }
+                            for (element in mx4.body.results) {
+                                allPlaces.add(element)
+                            }
+                            message.reply([results: allPlaces]);
+                            //
+                        }
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    def stop() {
+        container.logger.error(this.class.name + "has been stopped");
+    }
+
+    @Override
+    def generateJsonFile(ArrayList array) {
         return null
     }
 
     @Override
-    def generateCSVFile() {
-        return null
+    def generateCSVFile(ArrayList array) {
+        String csvFileContent
+
+        List keys = []
+        List values = []
+        for (element in array[0]) {
+            keys.add(element.key)
+        }
+        csvFileContent = "${keys.join(",")}\n"
+
+        for (element in array) {
+            values.clear()
+            for(property in keys) {
+                values.add(element."$property" ?: " ")
+            }
+            csvFileContent += "${values.join(",")}\n"
+        }
+
+        return [text: csvFileContent, size: csvFileContent.length()]
     }
 }
