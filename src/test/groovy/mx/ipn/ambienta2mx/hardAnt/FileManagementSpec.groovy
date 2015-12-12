afd3d03a68af6b218ab38788729c56c467534bcd
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-12-12 00:25:59 -0600

    #7, FileManagement test was updated, file's test passes

--- src/test/groovy/mx/ipn/ambienta2mx/hardAnt/FileManagementSpec.groovy
+++ src/test/groovy/mx/ipn/ambienta2mx/hardAnt/FileManagementSpec.groovy
@@ -1,9 +1,8 @@
 package mx.ipn.ambienta2mx.hardAnt
 
 import mx.ipn.ambienta2mx.hardAnt.services.PollutionService
-import mx.ipn.ambienta2mx.hardAnt.services.WeatherService
+import mx.ipn.ambienta2mx.hardAnt.services.DataTypeService
 import mx.ipn.ambienta2mx.hardAnt.services.api.FileManagement
-import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 import java.lang.Void as Should
@@ -15,7 +14,7 @@ class FileManagementSpec extends Specification {
     @Shared service = null
     Should "generate a csv file from given object's array (Weather)"() {
         given:
-        service = (FileManagement) new WeatherService()
+        service = (FileManagement) new DataTypeService()
         expect:
         service.generateCSVFile(array).text == fileContent
         where:
