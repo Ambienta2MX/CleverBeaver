35ce2e6df49c5e35f9c62c817b22384c7de74ca8
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-12-12 00:25:25 -0600

    #7, Updating verticles, now it enables a database query via url parameter

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/DataTypeVerticle.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/DataTypeVerticle.groovy
@@ -1,6 +1,6 @@
 package mx.ipn.ambienta2mx.hardAnt.verticles
 
-import mx.ipn.ambienta2mx.hardAnt.verticles.routes.WeatherRouter
+import mx.ipn.ambienta2mx.hardAnt.verticles.routes.DataTypeRouter
 import org.vertx.groovy.core.http.RouteMatcher
 import org.vertx.groovy.platform.Verticle
 
@@ -11,25 +11,25 @@ class DataTypeVerticle extends Verticle {
     Map definedConfiguration
     def server
     RouteMatcher routeMatcher
-    WeatherRouter weatherRouter
+    DataTypeRouter dataTypeRouter
 
     def start() {
-        container.logger.info("Weather Verticle has deployed");
+        container.logger.info("DataType Verticle has deployed");
         //
         definedConfiguration = container.getConfig()
         server = vertx.createHttpServer()
         routeMatcher = new RouteMatcher()
         //
-        weatherRouter =  new WeatherRouter()
-        weatherRouter.definedConfiguration = this.definedConfiguration
-        weatherRouter.eventBus = vertx.eventBus
-        weatherRouter.container = this.container
+        dataTypeRouter =  new DataTypeRouter()
+        dataTypeRouter.definedConfiguration = this.definedConfiguration
+        dataTypeRouter.eventBus = vertx.eventBus
+        dataTypeRouter.container = this.container
         //
-        routeMatcher.post("/weather",weatherRouter.saveWeather)
-        routeMatcher.get("/weather", weatherRouter.findWeatherBy)
-        routeMatcher.options("/weather", weatherRouter.findWeatherBy)
+        routeMatcher.post("/:dataType",dataTypeRouter.saveModel)
+        routeMatcher.get("/:dataType", dataTypeRouter.findDataTypeBy)
+        routeMatcher.options("/:dataType", dataTypeRouter.findDataTypeBy)
 
         //
-        server.requestHandler(routeMatcher.asClosure()).listen(definedConfiguration.weatherVerticle.http.port, definedConfiguration.weatherVerticle.http.host);
+        server.requestHandler(routeMatcher.asClosure()).listen(definedConfiguration.DataTypeVerticle.http.port, definedConfiguration.DataTypeVerticle.http.host);
     }
 }
--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/routes/DataTypeRouter.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/verticles/routes/DataTypeRouter.groovy
@@ -2,7 +2,6 @@ package mx.ipn.ambienta2mx.hardAnt.verticles.routes
 
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
-import mx.ipn.ambienta2mx.hardAnt.services.WeatherService
 
 /**
  * Created by alberto on 16/10/15.
@@ -11,30 +10,29 @@ class DataTypeRouter {
     def definedConfiguration
     def container
     def eventBus
-    WeatherService weatherService
 
-    def saveWeather = { request ->
+    def saveModel = { request ->
         request.bodyHandler { body ->
-            def weatherMap = new JsonSlurper().parseText("$body")
+            def modelMap = new JsonSlurper().parseText("$body")
             def fastEagleService = definedConfiguration.fastEagleService
             String url = fastEagleService.host + ":" + fastEagleService.port + fastEagleService.longitudeLatitudeService
-            url = url.replace(":latitude", "$weatherMap.latitude")
-            url = url.replace(":longitude", "$weatherMap.longitude")
+            url = url.replace(":latitude", "$modelMap.latitude")
+            url = url.replace(":longitude", "$modelMap.longitude")
             url = url.replace(":distance", "${request.params.distance ?: 100}")
             def urlObject = new URL(url)
             def place = new JsonSlurper().parse(urlObject)
             if (place) {
-                def mongoOperation = [action: 'save', collection: 'Weather']
-                weatherMap.location = place[0].location;
-                weatherMap.fullName = place[0].fullName
-                weatherMap.remove("latitude")
-                weatherMap.remove("longitude")
-                mongoOperation.document = weatherMap;
+                def mongoOperation = [action: 'save', collection: request.params.dataType.capitalize()]
+                modelMap.location = place[0].location;
+                modelMap.fullName = place[0].fullName
+                modelMap.remove("latitude")
+                modelMap.remove("longitude")
+                mongoOperation.document = modelMap;
                 def database = definedConfiguration.states[place[0].state];
                 eventBus.send("${definedConfiguration.databasesAddress}.${database}", mongoOperation) { result ->
                     request.response.putHeader("Content-Type", "application/json")
                     if (result) {
-                        request.response.end("${JsonOutput.toJson(weatherMap)}")
+                        request.response.end("${JsonOutput.toJson(modelMap)}")
                     } else {
                         request.response.code = 404;
                         request.response.end("{'save':false}")
@@ -45,7 +43,7 @@ class DataTypeRouter {
         }
     }
 
-    def findWeatherBy = { request ->
+    def findDataTypeBy = { request ->
         /*Enabling CORS*/
         request.response.putHeader("Access-Control-Allow-Origin", "${request.headers.origin}")
         request.response.putHeader("Access-Control-Allow-Methods", "GET, OPTIONS, POST");
@@ -54,9 +52,9 @@ class DataTypeRouter {
         def response
         try {
             if (request.params.name) {
-                response = this.findWeatherByPlaceName(request)
+                response = this.findDataTypeByPlaceName(request)
             } else {
-                response = this.findWeatherByLatLon(request)
+                response = this.findDataTypeByLatLon(request)
             }
             return response
         } catch (Exception e) {
@@ -67,12 +65,16 @@ class DataTypeRouter {
         }
     }
 
-    def findWeatherByLatLon = { request ->
+    def findDataTypeByLatLon = { request ->
         def fastEagleService = definedConfiguration.fastEagleService
         String url = fastEagleService.host + ":" + fastEagleService.port + fastEagleService.longitudeLatitudeService
         url = url.replace(":latitude", "$request.params.latitude")
         url = url.replace(":longitude", "$request.params.longitude")
         url = url.replace(":distance", "100") // for search purposes
+        println url
+        println request.params.dataType
+        println request.params.dataType
+        println request.params.dataType
         def coordinates = [Double.parseDouble(request.params.longitude ?: "0"), Double.parseDouble(request.params.latitude ?: "0")]
         def maxDistance = Integer.parseInt(request.params.distance ?: "100")
         def maxItems = Integer.parseInt(request.params.max ?: "10")
@@ -80,7 +82,7 @@ class DataTypeRouter {
         def place = new JsonSlurper().parse(urlObject)
         if (place) {
             def query = [
-                    action    : 'find', collection: 'Weather',
+                    action    : 'find', collection: request.params.dataType.capitalize(),
                     matcher   : [
                             location: [
                                     '$near': [
@@ -92,18 +94,18 @@ class DataTypeRouter {
                     limit     : maxItems,
                     sort_query: [sampleDate: -1]
             ]
-            eventBus.send("${definedConfiguration.WeatherFinder.address}", query) { message ->
+            eventBus.send("${definedConfiguration.DataTypeFinder.address}", query) { message ->
                 println("Resolving Information from $coordinates")
                 if (message.body) {
-                    request.response.end("${JsonOutput.toJson(message.body.results)}")
+                    return request.response.end("${JsonOutput.toJson(message.body.results)}")
                 } else {
-                    request.response.end("${JsonOutput.toJson([])}")
+                    return request.response.end("${JsonOutput.toJson([])}")
                 }
             }
         }
     }
 
-    def findWeatherByPlaceName = { request ->
+    def findDataTypeByPlaceName = { request ->
         def fastEagleService = definedConfiguration.fastEagleService
         String url = fastEagleService.host + ":" + fastEagleService.port + fastEagleService.nameService
         url = url.replace(":name", "${URLEncoder.encode(request.params.name)}")
@@ -117,7 +119,7 @@ class DataTypeRouter {
         def coordinates = [place[0].location.coordinates[0], place[0].location.coordinates[1]]
         if (place) {
             def query = [
-                    action    : 'find', collection: 'Weather',
+                    action    : 'find', collection: request.params.dataType.capitalize(),
                     matcher   : [
                             location: [
                                     '$near': [
@@ -130,12 +132,12 @@ class DataTypeRouter {
                     sort_query: [sampleDate: -1]
             ]
 
-            eventBus.send("${definedConfiguration.WeatherFinder.address}", query) { message ->
-                println("Resolving Weather Information from $coordinates, using place name")
+            eventBus.send("${definedConfiguration.DataTypeFinder.address}", query) { message ->
+                println("Resolving DataType Information from $coordinates, using place name")
                 if (message.body) {
-                    request.response.end("${JsonOutput.toJson(message.body.results)}")
+                    return request.response.end("${JsonOutput.toJson(message.body.results)}")
                 } else {
-                    request.response.end("${JsonOutput.toJson([])}")
+                    return request.response.end("${JsonOutput.toJson([])}")
                 }
             }
         }
