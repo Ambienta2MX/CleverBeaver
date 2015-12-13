8a7078c806b4b4f06b00ebe1e67cd6b44ff7ecca
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-12-13 02:01:40 -0600

    #8, Adding date filtering process, waiting for integration with SmartOwl

--- src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/DataTypeService.groovy
+++ src/main/groovy/mx/ipn/ambienta2mx/hardAnt/services/DataTypeService.groovy
@@ -21,25 +21,16 @@ class DataTypeService extends Verticle implements FileManagement {
         eventBus = vertx.eventBus
         eventBus.registerHandler("$definedConfiguration.DataTypeFinder.address") { message ->
             println "DataType Finder Verticle working";
-            eventBus.send("${definedConfiguration.databasesAddress}.MX1", message.body) { mx1 ->
-                eventBus.send("${definedConfiguration.databasesAddress}.MX2", message.body) { mx2 ->
-                    eventBus.send("${definedConfiguration.databasesAddress}.MX3", message.body) { mx3 ->
-                        eventBus.send("${definedConfiguration.databasesAddress}.MX4", message.body) { mx4 ->
+            eventBus.send("${definedConfiguration.databasesAddress}.MX1", message.body.query) { mx1 ->
+                eventBus.send("${definedConfiguration.databasesAddress}.MX2", message.body.query) { mx2 ->
+                    eventBus.send("${definedConfiguration.databasesAddress}.MX3", message.body.query) { mx3 ->
+                        eventBus.send("${definedConfiguration.databasesAddress}.MX4", message.body.query) { mx4 ->
                             List allPlaces = []
-                            for (element in mx1.body.results) {
-                                allPlaces.add(element)
-                            }
-                            for (element in mx2.body.results) {
-                                allPlaces.add(element)
-                            }
-                            for (element in mx3.body.results) {
-                                allPlaces.add(element)
-                            }
-                            for (element in mx4.body.results) {
-                                allPlaces.add(element)
-                            }
+                            allPlaces.addAll(latestFilter(mx1.body.results, message.body.latest))
+                            allPlaces.addAll(latestFilter(mx2.body.results, message.body.latest))
+                            allPlaces.addAll(latestFilter(mx3.body.results, message.body.latest))
+                            allPlaces.addAll(latestFilter(mx4.body.results, message.body.latest))
                             message.reply([results: allPlaces]);
-                            //
                         }
                     }
                 }
@@ -114,5 +105,21 @@ class DataTypeService extends Verticle implements FileManagement {
         } else {
             return [text: "", size: 0]
         }
+    }
+
+    List latestFilter(List elements, boolean enabled) {
+        if (enabled) {
+            use(TimeCategory) {
+                Date currentDate = new Date();
+                return elements.findAll { element ->
+                    //TODO verify date format before saving it into the database
+                    Date expirationDate = element.sampleDate + definedConfiguration.expirationHour.hours
+                    return expirationDate < currentDate
+                }
+            }
+        } else {
+            return elements
+        }
+
     }
 }
